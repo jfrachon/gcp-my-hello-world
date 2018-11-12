edcf88bd3d86caa94bf96001b652fef717f0cf7e
Jérémie Frachon <jfrachon@outlook.fr>
2018-11-12 16:31:34 +0100

    add php files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Personal pictures loader</h1>
+
+        <?php
+        echo "Hi, please enter your name!";
+        ?>
+
+        <form action="name.php" method="post">
+            Name: <input type="text" name="name"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
